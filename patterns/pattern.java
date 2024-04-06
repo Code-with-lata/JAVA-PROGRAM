1ae34e165c6879f29d3bd410ed18064388d4fd8c
Lata <prajapatilata01@gmail.com>
2024-04-06 22:05:23 +0530

    add

--- patterns/pattern.java
+++ patterns/pattern.java
@@ -6,9 +6,9 @@ public class pattern
         {
             for(int j=1;j<=i;j++)
             {
-               System.out.print( " " );
+               System.out.print( " *" );
             }
-            System.out.println(" ") ;      
+            System.out.println(" *") ;      
          }
     }
     
--- patterns/pattern5.java
+++ patterns/pattern5.java
@@ -1,10 +1,17 @@
 public class pattern5
 {
-    public static void main(String[ ]aegs)
-    {
-        for(int i=5; i>=1;i--)
+    public static void main(String[ ]args){
+        // int row =8;
+        // int column = 4;
+        for(int i=1; i<=5;i++)
         {
-            for(int j=1; j)
+            for(int j=1; j<=5-i;j++){
+                System.out.print(" ");
+            }
+            for(int k =1;k<=2*i-1;k++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
         }
     }
     
