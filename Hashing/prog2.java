59e77ac584bb1595e628a16a22fa2849f463cee7
Lata <prajapatilata01@gmail.com>
2024-04-07 22:06:08 +0530

    add

---
+++ Hashing/prog2.java
@@ -0,0 +1,15 @@
+//How to find the frequency in the Hashmap
+import java.util.HashMap;
+import java.util.Map;
+
+public class prog2 {
+    public static void main(String[] args) {
+        int [] arr ={1,2,2,1,4,3,2,1,2,1,6,9,6};
+        Map<Integer,Integer>map = new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i],map.getOrDefault(arr[i], 0)+1); 
+   }
+   System.out.println(map);
+    }
+    
+}
