7809e6d271f524472c2ef3001616b3d126ddf28e
Lata <prajapatilata01@gmail.com>
2024-04-07 21:51:40 +0530

    add

---
+++ Hashing/prog1.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class prog1 {
+    public static void main(String[] args) {
+        //TODO Auto_generated method stub
+        Map<Integer,Integer>map = new HashMap<>();
+        //how to put the value in hashMap
+        map.put(1,1);
+        map.put(2,4);
+        map.put(3,9);
+        //for update the value also used put
+        map.put(2,90);
+        //get method is used yo find the value
+        int val = map.getOrDefault(12, 0);
+        for(Integer key:map.keySet()){
+        System.out.println(map);
+        }
+    }
+
+    
+}
